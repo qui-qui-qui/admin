497e513d1e96dc8b055d427c631c1c2aed502df1
Daniil Rodionov <zenitpv@icloud.com>
2023-01-25 16:40:15 +0300

    add iframe

--- app/src/components/editor/editor.js
+++ app/src/components/editor/editor.js
@@ -1,61 +1,79 @@
+import "../../helpers/iframeLoader.js";
 import axios from "axios";
 import React, { Component } from "react";
 
 export default class Editor extends Component {
-  constructor() {
-    super();
+    constructor() {
+        super();
+        this.currentPage = "index.html";
+        this.state = {
+            pageList: [],
+            newPageName: "",
+        };
 
-    this.state = {
-      pageList: [],
-      newPageName: "",
-    };
+        this.createNewPage = this.createNewPage.bind(this);
+    }
 
-    this.createNewPage = this.createNewPage.bind(this);
-  }
+    componentDidMount() {
+        this.init(this.currentPage);
+    }
 
-  componentDidMount() {
-    this.loadPageList();
-  }
+    init(page) {
+        this.iframe = document.querySelector('iframe');
+        this.open(page);
+        this.loadPageList();
+    }
 
-  loadPageList() {
-    axios.get("./api").then((res) => this.setState({ pageList: res.data }));
-  }
+    open(page) {
+        this.currentPage = `../${page}`;
+        this.iframe.load(this.currentPage, () => {
+            console.log(this.currentPage);
+        });
+    }
 
-  createNewPage() {
-    axios
-      .post("./api/createNewPage.php", { name: this.state.newPageName })
-      .then(this.loadPageList())
-      .catch(() => alert("Страница уже существует"));
-  }
+    loadPageList() {
+        axios.get("./api").then((res) => this.setState({ pageList: res.data }));
+    }
 
-  deletePage(page) {
-    axios   
-        .post("./api/deletePage.php", {"name": page}) 
-        .then(this.loadPageList())
-        .catch(() => alert("Страницы не существует"));
-  }
+    createNewPage() {
+        axios
+            .post("./api/createNewPage.php", { name: this.state.newPageName })
+            .then(this.loadPageList())
+            .catch(() => alert("Страница уже существует"));
+    }
 
-  render() {
-    const { pageList } = this.state;
-    const pages = pageList.map((page, i) => {
-      return (
-        <h1 key={i}>
-          {page}
-          <a href="#" onClick={() => this.deletePage(page)}>х</a>
-        </h1>
-      );
-    });
-    return (
-      <>
-        <input
-          type="text"
-          onChange={(e) => {
-            this.setState({ newPageName: e.target.value });
-          }}
-        />
-        <button onClick={this.createNewPage}>Создать страницу</button>
-        {pages}
-      </>
-    );
-  }
+    deletePage(page) {
+        axios
+            .post("./api/deletePage.php", { name: page })
+            .then(this.loadPageList())
+            .catch(() => alert("Страницы не существует"));
+    }
+
+    render() {
+        // const { pageList } = this.state;
+        // const pages = pageList.map((page, i) => {
+        //     return (
+        //         <h1 key={i}>
+        //             {page}
+        //             <a href="#" onClick={() => this.deletePage(page)}>
+        //                 X
+        //             </a>
+        //         </h1>
+        //     );
+        // });
+        
+        return (
+            <iframe src={this.currentPage} frameBorder="0"></iframe>
+            // <>
+            //     <input
+            //         type="text"
+            //         onChange={(e) => {
+            //             this.setState({ newPageName: e.target.value });
+            //         }}
+            //     />
+            //     <button onClick={this.createNewPage}>Создать страницу</button>
+            //     {pages}
+            // </>
+        )
+    }
 }
